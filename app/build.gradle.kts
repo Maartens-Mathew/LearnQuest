55776fc13f1aec8682ab4aa32e4bc5a68ba52a06
Granville <149877442+PartialCode@users.noreply.github.com>
2024-07-27 10:35:19 +0200

    Added Vico Chart library and created ViewProgressActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -40,4 +41,19 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    //vico chart libraries
+    implementation("com.patrykandpatrick.vico:compose:2.0.0-alpha.25")
+
+    // For `compose`. Creates a `ChartStyle` based on an M2 Material Theme.
+    implementation("com.patrykandpatrick.vico:compose-m2:2.0.0-alpha.25")
+
+    // For `compose`. Creates a `ChartStyle` based on an M3 Material Theme.
+    implementation("com.patrykandpatrick.vico:compose-m3:2.0.0-alpha.25")
+
+    // Houses the core logic for charts and other elements. Included in all other modules.
+    implementation("com.patrykandpatrick.vico:core:2.0.0-alpha.25")
+
+    // For the view system.
+    implementation("com.patrykandpatrick.vico:views:2.0.0-alpha.25")
 }
