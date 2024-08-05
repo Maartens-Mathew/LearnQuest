c4318e245f69831705f64a22edf90fa4e87c9888
Granville <149877442+PartialCode@users.noreply.github.com>
2024-08-05 08:39:22 +0200

    made changes to chart on ViewProgessActivity

--- app/src/main/java/com/example/learnquest/Database.java
+++ app/src/main/java/com/example/learnquest/Database.java
@@ -1,28 +1,30 @@
-package com.example.learnquest.Utilities;
+package com.example.learnquest;
 
 import android.util.Log;
 import android.widget.Toast;
 
 import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Arrays;
+import java.util.Enumeration;
 
 public class Database {
     private Connection connection = null;
     private final String dTag = "Database";
     @Override
     protected void finalize() throws Throwable {
-        if (connection != null){
-            connection.close();
-        }
+//        if (connection != null){
+//            connection.close();
+//        }
         super.finalize();
     }
 
     public Database(){
         new Thread(() -> {
             try{
-                Class.forName("com.mysql.jdbc.Driver");
+                Log.i(dTag,Class.forName("com.mysql.jdbc.Driver").toString());
                 connection = DriverManager.getConnection("jdbc:mysql://project-sql-database-documentation4project.e.aivencloud.com:16222/defaultdb",
                         "avnadmin","AVNS_4DQayfBvqZRs5GSSp6m");
                 Log.i(dTag,"connected");
@@ -31,6 +33,11 @@ public class Database {
                 for(StackTraceElement element : e.getStackTrace()){
                     Log.e(dTag,element.toString());
                 }
+                Enumeration<Driver> drivers = DriverManager.getDrivers();
+                do{
+                    Driver d = drivers.nextElement();
+                    Log.e(dTag,d.getClass().toString());
+                }while (drivers.hasMoreElements());
             }
         }).start();
     }
--- app/src/main/java/com/example/learnquest/ViewProgessActivity.java
+++ app/src/main/java/com/example/learnquest/ViewProgessActivity.java
@@ -2,19 +2,9 @@ package com.example.learnquest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 public class ViewProgessActivity extends AppCompatActivity {
@@ -23,70 +13,74 @@ public class ViewProgessActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_progess);
-        try{
-            LineChart lineChart = findViewById(R.id.lineChart);
-            lineChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
-            LineData lineData = new LineData();
-            Random random = new Random();
-            int size = 5;
-            float[] idealMarks = new float[size];
-            float[] weights = new float[size];
-            float[] marksObtained = new float[size];
-            //I don't know why but must have List<Entry> pointer
-            List<Entry> currProgressData = new ArrayList<>(30);
-            List<Entry> goalMarkData = new ArrayList<>(11);
-            float remWeight = 1.0f;
-            float weightToInsert;
-            for (int i = 0; i < size; i++) {
-                idealMarks[i] = getValueInclusive(random);
-                marksObtained[i] = getValueInclusive(random);
-                do{
-                    weightToInsert = getWeight(random);
-                } while (remWeight - weightToInsert < 0.0f);
-                weights[i] = weightToInsert;
-                Entry e = new Entry();
-                e.setX((float)i);
-                if (i <= 2){
-                    e.setY(marksObtained[i]);
-                }
-                else{
-                    e.setY(idealMarks[i]);
-                }
-                Entry e1 = new Entry((float)i, idealMarks[i]);
-                Log.i("MainActivity",e.toString());
-                Log.i("MainActivity","e1: "+e1.toString());
-                currProgressData.add(e);
-                goalMarkData.add(e1);
-            }
-            float currProg = calcCurrentProgress(idealMarks,weights,marksObtained,3);
-            float idealMark = calcIdealMark(idealMarks,weights);
-            String currProgOutput = String.format("%.0f",currProg*100.0f);
-            String idealMarkOutput = String.format("%.0f",idealMark*100.0f);
-            TextView lblCurrProgress = findViewById(R.id.lblCurrentProgress);
-            TextView lblGoalMark = findViewById(R.id.lblGoalMark);
-            lblCurrProgress.setText(currProgOutput + "%");
-            lblGoalMark.setText(idealMarkOutput + "%");
-            LineDataSet ds = new LineDataSet(currProgressData,"current");
-            LineDataSet dx = new LineDataSet(goalMarkData, "ideal");
-            ds.setColor(Color.RED);
-            dx.setColor(Color.BLUE);
-            dx.setMode(LineDataSet.Mode.CUBIC_BEZIER);
-            ds.setMode(LineDataSet.Mode.CUBIC_BEZIER);
-            ds.setDrawValues(false);
-            dx.setDrawValues(false);
-            lineData.addDataSet(ds);
-            lineData.addDataSet(dx);
-            lineChart.setData(lineData);
-            lineChart.invalidate();
-        }
-        catch (Exception e){
-            if (e.getMessage() != null){
-                Log.e("MainActivity", e.getMessage());
-            }
-            for (StackTraceElement element : e.getStackTrace()){
-                Log.e("MainActivity",e.toString());
-            }
-        }
+        TextView tx = findViewById(R.id.lblGoalMark);
+        tx.setOnClickListener(view ->{
+            Database ds = new Database();
+        });
+//        try{
+//            LineChart lineChart = findViewById(R.id.lineChart);
+//            lineChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+//            LineData lineData = new LineData();
+//            Random random = new Random();
+//            int size = 5;
+//            float[] idealMarks = new float[size];
+//            float[] weights = new float[size];
+//            float[] marksObtained = new float[size];
+//            //I don't know why but must have List<Entry> pointer
+//            List<Entry> currProgressData = new ArrayList<>(30);
+//            List<Entry> goalMarkData = new ArrayList<>(11);
+//            float remWeight = 1.0f;
+//            float weightToInsert;
+//            for (int i = 0; i < size; i++) {
+//                idealMarks[i] = getValueInclusive(random);
+//                marksObtained[i] = getValueInclusive(random);
+//                do{
+//                    weightToInsert = getWeight(random);
+//                } while (remWeight - weightToInsert < 0.0f);
+//                weights[i] = weightToInsert;
+//                Entry e = new Entry();
+//                e.setX((float)i);
+//                if (i <= 2){
+//                    e.setY(marksObtained[i]);
+//                }
+//                else{
+//                    e.setY(idealMarks[i]);
+//                }
+//                Entry e1 = new Entry((float)i, idealMarks[i]);
+//                Log.i("MainActivity",e.toString());
+//                Log.i("MainActivity","e1: "+e1.toString());
+//                currProgressData.add(e);
+//                goalMarkData.add(e1);
+//            }
+//            float currProg = calcCurrentProgress(idealMarks,weights,marksObtained,3);
+//            float idealMark = calcIdealMark(idealMarks,weights);
+//            String currProgOutput = String.format("%.0f",currProg*100.0f);
+//            String idealMarkOutput = String.format("%.0f",idealMark*100.0f);
+//            TextView lblCurrProgress = findViewById(R.id.lblCurrentProgress);
+//            TextView lblGoalMark = findViewById(R.id.lblGoalMark);
+//            lblCurrProgress.setText(currProgOutput + "%");
+//            lblGoalMark.setText(idealMarkOutput + "%");
+//            LineDataSet ds = new LineDataSet(currProgressData,"current");
+//            LineDataSet dx = new LineDataSet(goalMarkData, "ideal");
+//            ds.setColor(Color.RED);
+//            dx.setColor(Color.BLUE);
+//            dx.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+//            ds.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+//            ds.setDrawValues(false);
+//            dx.setDrawValues(false);
+//            lineData.addDataSet(ds);
+//            lineData.addDataSet(dx);
+//            lineChart.setData(lineData);
+//            lineChart.invalidate();
+//        }
+//        catch (Exception e){
+//            if (e.getMessage() != null){
+//                Log.e("MainActivity", e.getMessage());
+//            }
+//            for (StackTraceElement element : e.getStackTrace()){
+//                Log.e("MainActivity",e.toString());
+//            }
+//        }
     }
 
     public float calcCurrentProgress(float[] idealMark, float[] weights, float[] markObtained, int n){
