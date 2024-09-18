850ea2235fdea75406158917200f5c049a000742
Granville <149877442+PartialCode@users.noreply.github.com>
2024-09-18 11:27:59 +0200

    Had to redo entire branch to undo changes I made to nav bar fragments

--- app/src/main/java/com/example/learnquest/Utils/database/SupabaseApi.java
+++ app/src/main/java/com/example/learnquest/Utils/database/SupabaseApi.java
@@ -2,6 +2,7 @@ package com.example.learnquest.Utils.database;
 
 
 import com.example.learnquest.LoginRegister.Dashboard.SelectGroups.GroupDashboard.Assessments.Assessment;
+import com.example.learnquest.LoginRegister.Dashboard.SelectGroups.GroupDashboard.Assessments.TrackProgressAssessmentData;
 import com.example.learnquest.LoginRegister.Dashboard.SelectGroups.GroupDashboard.Quiz.QuizBank.QuizEntry;
 import com.example.learnquest.model.user.User;
 
@@ -75,6 +76,7 @@ public interface SupabaseApi {
     Call<Void> deleteQuizEntry(@Query("quizEntryID") String quizEntryID);//narsi delete eq for a given id (working)
 
 
+    Call<List<TrackProgressAssessmentData>> get_assessment_data(@Query("user_id") int userID, @Query("group_id") int groupID);
 
 
 
