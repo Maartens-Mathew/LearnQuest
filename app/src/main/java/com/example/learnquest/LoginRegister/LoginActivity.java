a26a201ead476885e8891f278af7d995448eaf50
Mathew <s225588994@mandela.ac.za>
2024-10-12 18:13:24 +0200

    Incorrect branch, will re-merge.

--- app/src/main/java/com/example/learnquest/LoginRegister/LoginActivity.java
+++ app/src/main/java/com/example/learnquest/LoginRegister/LoginActivity.java
@@ -1,26 +1,43 @@
 package com.example.learnquest.LoginRegister;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewbinding.ViewBinding;
 
 import com.example.learnquest.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText edtUsername, edtPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        edtUsername = findViewById(R.id.edt_login_username);
+        edtPassword = findViewById(R.id.edt_login_password);
+
+
+    }
+
+    public void onLoginClick(View view) {
+
+
+
     }
 }
